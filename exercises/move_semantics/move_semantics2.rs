099ffef83493533ac410dbc3fba8799f6b6403f1
Ciro Della Volpe <cirodvolpe@gmail.com>
2022-03-14 12:48:29 -0300

    move_semantics2 done all 3 solutions

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -4,25 +4,70 @@
 
 // I AM NOT DONE
 
-fn main() {
-    let vec0 = Vec::new();
+// First solution
+// fn main() {
+//     let vec0 : Vec<i32> = Vec::new();
+//     let other_vec = Vec::new();
+//     let mut vec1 = fill_vec(other_vec);
 
-    let mut vec1 = fill_vec(vec0);
+//     // Do not change the following line!
+//     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
+
+//     vec1.push(88);
+
+//     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
+// }
+
+// fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+//     let mut vec = vec;
+
+//     vec.push(22);
+//     vec.push(44);
+//     vec.push(66);
+
+//     vec
+// }
+
+// Second Solution
+// fn main() {
+//     let vec0 = Vec::new();
+
+//     let mut vec1 = fill_vec(&vec0);
+
+//     // Do not change the following line!
+//     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
+
+//     vec1.push(88);
+
+//     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
+// }
+
+// fn fill_vec(vec: &Vec<i32>) -> Vec<i32> {
+//     let mut vec = vec.clone();
+
+//     vec.push(22);
+//     vec.push(44);
+//     vec.push(66);
+
+//     vec
+// }
+
+// Third Solution
+fn main() {
+    let mut vec0 = Vec::new();
+
+    fill_vec(&mut vec0);
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
 
-    vec1.push(88);
+    vec0.push(88);
 
-    println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
+    println!("{} has length {} content `{:?}`", "vec0 with push", vec0.len(), vec0);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
-
+fn fill_vec(vec: &mut Vec<i32>) {
     vec.push(22);
     vec.push(44);
     vec.push(66);
-
-    vec
 }
